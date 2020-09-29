0574c7d2ec1c39a8fd8200b2ba348153c847291e
Beth Presten <bethpresten@gmail.com>
2020-09-29 13:14:25 -0400

    timer working upon start of quiz

--- script.js
+++ script.js
@@ -1,60 +1,58 @@
+//pulling in variables from html
 // console.log("Connection is working!")
 var welcomeContainer = document.getElementById("welcome-container");
 var quizButton = document.getElementById("start-quiz");
 var answerContainer = document.getElementById("answer-container");
 var questionContainer = document.getElementById("question-container");
-var correctAnswer = document.getElementById("correct-answer");
+var timerEl = document.getElementById("quiz-timer");
 
-
-// var timerDisplay = document.createElement("quiz-timer")
 var questionIndex = 0;
+
+//javascript variables
 
 var quizBank = [
     {
         question: "Commonly used data types DO NOT include:",
         choices: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
-        answer: "alerts",
+        answer: "3. alerts",
     },
     {
         question: "The condition in an if / else statement is enclosed within ________.",
         choices: ["1. quotes", "2. curly brackets", "3. parentheses", "4. square brackets"],
-        answer: "curly brackets",
+        answer: "2. curly brackets",
     },
     {
         question: "Arrays in JavaScript can be used to store _____.",
         choices: ["1. numbers and strings", "2. other arrays", "3. booleans", "4. all of the answers are true"],
-        answer: "all of the answers are true",
+        answer: "4. all of the answers are true",
     },
     {
         question: "String values must be enclosed within _______ when being assigned to variables.",
         choices: ["1. commas", "2. curly brackets", "3. quotes", "4. parentheses"],
-        answer: "quotes",
+        answer: "3. quotes",
     },
     {
         question: "A very useful tool used during development and debugging for printing content to the debugger is:",
         choices: ["1. JavaScript", "2. terminal/bash", "3. for loops", "4. console log"],
-        answer: "console log",
+        answer: "4. console log",
     },
 ]
-
-
 
 function renderQuizBank() {
     questionContainer.innerHTML = "";
     var question = document.createElement("h2");
     question.textContent = quizBank[questionIndex].question;
     // questions now need to go in questionContainer
-    console.log(quizBank[questionIndex].question);
+    // console.log(quizBank[questionIndex].question);
     questionContainer.append(question)
     renderChoices();
-
 }
 
 function renderChoices() {
     // console.log(choices);
     //empty the former answers out of the container and re-populate with the next answer set
     answerContainer.innerHTML = "";
-    for (var i = 0; i <= quizBank[questionIndex].choices.length; i++) {
+    for (var i = 0; i < quizBank[questionIndex].choices.length; i++) {
         var choiceButtons = document.createElement("button");
         choiceButtons.setAttribute("style", "background-color: indigo; color: white; margin: 5px");
         choiceButtons.setAttribute("data-value", quizBank[questionIndex].choices[i]);
@@ -63,28 +61,23 @@ function renderChoices() {
     }
 }
 
-function correctAnswer() {
-    correctAnswer.setAttribute("data-value", quizBank[questionIndex].correctAnswer)
-    // if ()
-}
-
 answerContainer.addEventListener("click", function (event) {
     var element = event.target;
-    console.log(element);
-    var correctAnswer = quizBank[questionIndex].answer
+    // console.log(element);
+    var correctAnswer = quizBank[questionIndex].answer;
     // If that element is a button...
     if (element.matches("button") === true) {
         var answer = element.getAttribute("data-value");
         var createDiv = document.createElement("div");
+        createDiv.innerHTML = "";
 
-        if (correctAnswer === answer) {
-            createDiv.textContent = "Correct!";
+        if (answer === correctAnswer) {
+            // console.log(answer);
+            createDiv.textContent.innerHTML = "Correct!";
             questionIndex++;
-
-            console.log(answer);
             renderQuizBank();
         } else {
-            createDiv.textContent = "Incorrect!";
+            createDiv.textContent.innerHTML = "Incorrect!";
             questionIndex++;
             renderQuizBank();
 
@@ -92,43 +85,37 @@ answerContainer.addEventListener("click", function (event) {
 
         }
     }
-    console.log(questionIndex)
-    // // answerToDisplay.textContent = correctAnswer;
-    // answerContainer.append(createDiv);
-})
+    // console.log(quizBank[questionIndex].correctAnswer);
 
-
-quizButton.addEventListener("click", function () {
-    // console.log(quizButton, "You clicked!");
-    welcomeContainer.style.display = "none";
-    questionContainer.setAttribute("style", "display:block");
-    renderQuizBank();
 });
 
 
 
 
 
-//     answers.addEventListener("click", function (event) {
-//         if (event.target.matches("answer"){
-//             var correctAnswer = event.target.getAttribute("correct-answer");
+quizButton.addEventListener("click", function () {
+    // console.log(quizButton, "You clicked!");
+    welcomeContainer.style.display = "none";
+    questionContainer.setAttribute("style", "display:block");
+    var secondsLeft = 75;
+
+    // timer element
+    function setTime() {
+        var timerInterval = setInterval(function () {
+            secondsLeft--;
+            timerEl.textContent = secondsLeft;
+            if (secondsLeft === 0) {
+                clearInterval(timerInterval);
+            }
+        }, 1000);
+    }
+    setTime();
+
+    // timerEl.append(textContent)
+    // startQuiz();
+    renderQuizBank();
+
+});
 
 
 
-//         }
-//     }
-//     });
-
-
-    // function quizTimer() {
-    //     timerSeconds = 30;
-    //     var interval = setInterval(function () {
-    //         timerSeconds--;
-    //         timerDisplay.textContent = "Time: " + timerSeconds;
-
-    //         if (timerSeconds === 0) {
-    //             clearInterval(interval);
-    //             var timesUp = timerDisplay("Time's Up!")
-    //         }
-        // }, 1000);
-    // }
